8a9e53969fb85869a49ca0138d8e061e1a226629
ysh <1299331829@qq.com>
2025-03-17 18:45:53 +0800

    first commit

---
+++ config.el
@@ -0,0 +1,391 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "yoshiki"
+      user-mail-address "1299331829@qq.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 24 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Sarasa Term SC Nerd" :size 26))
+
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;;(setq doom-theme 'doom-one)
+;;启动主题设置
+;;(setq doom-theme 'adwaita)
+(setq doom-theme 'doom-dracula)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+
+;; 设置org文件的默认目录
+(setq org-directory "~/.org/")
+
+;;打开org的目录
+(defun open-org-dir ()
+        (interactive)
+        (find-file (read-file-name "Open file or directory: " "/home/yoshiki01/.org/")))
+(map! "C-c f o" #'open-org-dir)
+
+;;org-roam的数据库，文件改变以保证缓存一致性
+(org-roam-db-autosync-mode)
+
+;;org快捷键
+(map! :after org
+      "C-c M-w" #'+org/refile-to-file
+      "C-c C-w" #'org-refile
+      "C-c C-r" #'org-roam-refile
+      "C-c i i" #'yoshiki/org-insert-artist-drawing)
+
+;;粘贴图片
+(map! :after org-download
+      "C-M-y" #'org-download-clipboard)
+
+;;org noter的笔记buffer中滚动pdf的buffer
+(defun yoshiki/scroll-other-window ()
+  (interactive)
+  (let* ((wind (other-window-for-scrolling))
+         (mode (with-selected-window wind major-mode)))
+    (if (eq mode 'pdf-view-mode)
+        (with-selected-window wind
+          (pdf-view-next-line-or-next-page 2))
+      (scroll-other-window 2))))
+(defun yoshiki/scroll-other-window-down ()
+  (interactive)
+  (let* ((wind (other-window-for-scrolling))
+         (mode (with-selected-window wind major-mode)))
+    (if (eq mode 'pdf-view-mode)
+        (with-selected-window wind
+          (progn
+            (pdf-view-previous-line-or-previous-page 2)
+            (other-window 1)))
+      (scroll-other-window-down 2))))
+(map! :after org-noter
+      :map org-mode-map
+      "C-M-v" #'yoshiki/scroll-other-window
+      "C-M-S-v" #'yoshiki/scroll-other-window-down)
+
+;;加载ox-freemind
+(after! org
+ (require 'ox-freemind))
+
+;;debug时用的
+;; (add-variable-watcher 'org-agenda-files
+;;                       (lambda (_sym newval operation where)
+;;                         (message "org-agenda-files changed: %s (Operation: %s)" newval operation)
+;;                         (debug))) ; 捕获调用堆栈
+
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+'). 设置doom变量
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+
+
+
+
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+
+
+;;emacs开启时，自动最大化，开启自动换行
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+
+;;永久显示workspace
+(after! persp-mode
+  (defun display-workspaces-in-minibuffer ()
+    (with-current-buffer " *Minibuf-0*"
+      (erase-buffer)
+      (insert (+workspace--tabline))))
+  (run-with-idle-timer 1 t #'display-workspaces-in-minibuffer)
+  (+workspace/display))
+
+;;更改dashboard
+(defun my-weebery-is-always-greater ()
+  (let* ((banner '("Hello World"                                   ))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+(setq +doom-dashboard-ascii-banner-fn #'my-weebery-is-always-greater)
+
+;;修改doom-dashboard的footer
+(defun doom-dashboard-widget-footer ()
+  (insert
+   "\n"
+   (+doom-dashboard--center
+    (- +doom-dashboard--width 2)
+    (with-temp-buffer
+      (insert-text-button
+       (propertize "yoshiki" 'face 'doom-dashboard-footer)
+       'action (lambda (_) (browse-url "https://example.com"))
+       'follow-link t
+       'help-echo "Open custom page")
+      (buffer-string)))
+   "\n"))
+
+
+;;搜索增强
+(map! "C-s" #'consult-line)
+;;快速运行代码
+(map! "<f5>" #'quickrun)
+;;切换treemacs窗口
+(map! :prefix "C-c"
+      "w e" #'treemacs-select-window)
+;;增强切换buffer,会显示最近的文件
+(map! :prefix "C-x"
+      "b" #'switch-to-buffer)
+;;projectile相关设置
+(after! projectile
+  (map! :map projectile-mode-map "C-c p a" #'projectile-add-known-project)
+  (map! :map projectile-mode-map "C-c p r" #'projectile-remove-known-project))
+
+
+
+
+
+;;打开项目
+(defun open-proj ()
+  (interactive)
+  (find-file (read-file-name "open proj: " "/home/yoshiki01/.proj/")))
+(map! "C-c f j" #'open-proj)
+
+;;解绑快捷键
+;;(map! :map general-override-mode-map "C-c f p" nil)
+
+;;字体放大缩小
+(map! :map global-map
+      "C-=" #'doom/increase-font-size
+      "C--" #'doom/decrease-font-size)
+;;窗口调整
+(map! "M-]" #'doom/window-enlargen
+      "M-[" #'balance-windows)
+
+
+;;avy-goto-word-0快捷键
+(map! :map global-map
+      "C-r" #'avy-goto-word-0)
+;;avy-goto-line快捷键
+(map! "C-x j" #'avy-goto-line)
+
+;;dired改键
+(map! :after dired
+      :map dired-mode-map
+      "b" #'dired-up-directory)
+
+;;imenu-list配置
+(map! "C-c o i" #'imenu-list)
+(setq! imenu-list-auto-resize t)
+
+;;imenu-list快捷键设置
+(defvar my-toggle-state t)
+(defun yoshiki/my-toggle-show-hide ()
+  "Toggle between `hs-show-all` and `hs-hide-all` functions."
+  (interactive)
+  (if (not (fboundp 'hs-show-all))
+      (message "The `hs-show-all` function is not defined.")
+    (if (not (fboundp 'hs-hide-all))
+        (message "The `hs-hide-all` function is not defined.")
+      (if my-toggle-state
+          (progn
+            (setq my-toggle-state nil)
+            (message "Hiding...")
+            (funcall #'hs-hide-all))
+        (progn
+          (setq my-toggle-state t)
+          (message "Showing...")
+          (funcall #'hs-show-all))))))
+(map! :after imenu-list
+      :map imenu-list-major-mode-map
+      "u" #'yoshiki/my-toggle-show-hide)
+
+(map! :map c-mode-base-map "C-o" #'lsp-ui-doc-toggle)
+;; (after! lsp-ui
+;;   (setq lsp-ui-doc-max-width 200
+;;         lsp-ui-doc-max-height 100
+;;         lsp-ui-doc-position 'top))
+;; (custom-set-faces
+;;  '(lsp-ui-doc ((t (:height 1.0 :family "Courier New"))))) ;; 设置字体和大小
+
+
+;;当子任务全部DONE时，父任务自动转变为DONE
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-todo-log-states)
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(add-hook 'org-after-todo-statistics-hook #'org-summary-todo)
+
+;;当TODO状态变成STRT时，如果不存在计时器，则开始计时
+(defun yoshiki/org-clock-in-when-strt ()
+  (when (string= (org-get-todo-state) "STRT")
+    (when (not (org-clock-is-active))
+      (org-clock-in))))
+(add-hook 'org-after-todo-state-change-hook #'yoshiki/org-clock-in-when-strt)
+
+;;当其他状态转变为TODO时，检查是否存在计时器，存在就关闭
+(defun yoshiki/org-clock-out-when-todo ()
+  (when (string= (org-get-todo-state) "TODO")
+    (when (org-clock-is-active)
+      (org-clock-out))))
+(add-hook 'org-after-todo-state-change-hook #'yoshiki/org-clock-out-when-todo)
+
+
+(after! org
+  (setq! tab-width 8))
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (setq c-basic-offset 4)))
+
+
+;;artist改键
+(map! :after artist
+      :map artist-mode-map
+      "C-c C-c" nil
+      "C-c C-'" #'artist-mode-off
+      "C-r" #'artist-select-op-square
+      "C-s" #'artist-select-op-line
+      "C-w" #'artist-select-op-cut-square
+      "C-y" #'artist-select-op-paste
+      "M-w" #'artist-select-op-copy-rectangle
+      "C-b" #'picture-backward-column
+      "<left>" #'picture-backward-column
+      "C-t" #'artist-toggle-second-arrow)
+
+
+
+;;org-mode中插入artist绘画
+(defun yoshiki/org-insert-artist-drawing ()
+  "Create a new buffer for drawing in artist-mode. Insert the drawing into the current Org line upon completion."
+  (interactive)
+  (unless (derived-mode-p 'org-mode)
+    (error "This function can only be used in Org Mode"))
+  (let ((artist-buffer (generate-new-buffer "*Artist Drawing*"))
+        (current-buffer (current-buffer))
+        (line-marker (point-marker))) ; Record the current line position
+    ;; Display the artist buffer in a window at the bottom and switch to it
+    (display-buffer artist-buffer '((display-buffer-below-selected)
+                                    (window-height . 0.3))) ; Adjust height as needed
+    (select-window (get-buffer-window artist-buffer)) ; Move cursor to the new buffer
+    (with-current-buffer artist-buffer
+      ;; Enable artist-mode
+      (artist-mode)
+      ;; Set up the keybinding for exiting, inserting into Org, and closing buffer
+      (local-set-key (kbd "C-c C-c")
+                     (lambda ()
+                       (interactive)
+                       (let ((drawing (buffer-string))) ; Get drawing content
+                         ;; Return to the original Org buffer
+                         (switch-to-buffer current-buffer)
+                         ;; Insert into the specified line
+                         (goto-char (marker-position line-marker))
+                         (insert drawing)
+                         ;; Delete the temporary buffer and close its window
+                         (kill-buffer artist-buffer)
+                         (delete-window (get-buffer-window artist-buffer)))))
+      ;; Set up the keybinding for cancelling (C-c C-k)
+      (local-set-key (kbd "C-c C-k")
+                     (lambda ()
+                       (interactive)
+                       ;; Return to the original Org buffer without inserting anything
+                       (switch-to-buffer current-buffer)
+                       ;; Delete the temporary buffer and close its window
+                       (kill-buffer artist-buffer)
+                       (delete-window (get-buffer-window artist-buffer)))))))
+
+;;滑动优化
+;; (map! "C-v" #'pixel-scroll-up)
+;; (map! "M-v" #'pixel-scroll-down)
+
+
+;;保存org文件时，自动更新修改时间
+(defun yoshiki/org-update-last-modified ()
+  "Update the LAST_MODIFIED property in the Org file."
+  (when (derived-mode-p 'org-mode)
+    (save-excursion
+      (goto-char (point-min))
+      (if (re-search-forward "^#\\+LAST_MODIFIED:.*$" nil t)
+          (replace-match (concat "#+LAST_MODIFIED: " (format-time-string "%Y-%m-%d %H:%M:%S")))
+        (goto-char (point-min))
+        (if (re-search-forward "^#\\+TITLE:.*$" nil t)
+            (progn
+              (end-of-line)
+              (insert (concat "\n#+LAST_MODIFIED: " (format-time-string "%Y-%m-%d %H:%M:%S") "\n")))
+          (insert (concat "#+TITLE: Untitled\n#+LAST_MODIFIED: " (format-time-string "%Y-%m-%d %H:%M:%S") "\n")))))))
+
+(add-hook 'before-save-hook 'yoshiki/org-update-last-modified)
+
+;;设置pdf-tools高亮快捷键
+(map! :after pdf-tools
+      :map pdf-annot-minor-mode-map
+      "C-SPC" #'pdf-annot-add-highlight-markup-annotation)
+
+;;设置进入.h文件，自动进入cpp-mode
+(after! cc-mode
+  (add-to-list 'auto-mode-alist '("\\.h\\'" . cpp-mode)))
+
+(map! "<drag-mouse-8>" #'better-jumper-jump-backward)
+(map! "C-t" #'transpose-frame)
+
+
---
+++ custom.el
@@ -0,0 +1,156 @@
+(put 'customize-themes 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(c-default-style '((java-mode . "java") (awk-mode . "awk") (other . "doom")))
+ '(custom-safe-themes
+   '("7f6bcac0505fb3468fcf436e1274dc538b789f642478320a5f95bfc07d8f10a1" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "6e33d3dd48bc8ed38fd501e84067d3c74dfabbfc6d345a92e24f39473096da3f" "77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" default))
+ '(display-time-mode t)
+ '(display-time-use-mail-icon nil)
+ '(elfeed-feeds '("https://emacs.liujiacai.net/podcast/index.xml"))
+ '(flycheck-buffer-switch-check-intermediate-buffers t)
+ '(flycheck-clang-args '("-std=c++17"))
+ '(flycheck-display-errors-delay 0.25)
+ '(flycheck-display-errors-function 'flycheck-popup-tip-show-popup)
+ '(flycheck-emacs-lisp-load-path 'inherit)
+ '(flycheck-idle-change-delay 1.0)
+ '(flycheck-indication-mode 'right-fringe)
+ '(flycheck-popup-tip-error-prefix "[!] ")
+ '(global-flycheck-mode t)
+ '(lsp-clients-clangd-args '("-j=4" "-j=4" "-j=4" "--header-insertion-decorators=0"))
+ '(lsp-ui-doc-show-with-mouse t)
+ '(org-agenda-dim-blocked-tasks nil)
+ '(org-agenda-files
+   '("~/.org/someday.org" "/home/yoshiki01/.org/inbox.org" "/home/yoshiki01/.org/agenda.org"))
+ '(org-capture-templates
+   '(("t" "Personal todo")
+     ("tl" "todo with link" entry
+      (file+headline "~/.org/inbox.org" "Inbox")
+      "* TODO [#B] %? [/]\12%a" :prepend t)
+     ("tt" "todo without link" entry
+      (file+headline "~/.org/inbox.org" "Inbox")
+      "* TODO [#B] %? [/]" :prepend t)
+     ("n" "Personal notes" entry
+      (file+headline +org-capture-notes-file "Inbox")
+      "* %u %?\12%i\12%a" :prepend t)
+     ("j" "Journal" entry
+      (file+olp+datetree +org-capture-journal-file)
+      "* %U %?\12%i\12%a" :prepend t)
+     ("p" "Templates for projects")
+     ("pt" "Project-local todo" entry
+      (file+headline +org-capture-project-todo-file "Inbox")
+      "* TODO %?\12%i\12%a" :prepend t)
+     ("pn" "Project-local notes" entry
+      (file+headline +org-capture-project-notes-file "Inbox")
+      "* %U %?\12%i\12%a" :prepend t)
+     ("pc" "Project-local changelog" entry
+      (file+headline +org-capture-project-changelog-file "Unreleased")
+      "* %U %?\12%i\12%a" :prepend t)
+     ("o" "Centralized templates for projects")
+     ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\12 %i\12 %a" :heading "Tasks" :prepend nil)
+     ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\12 %i\12 %a" :prepend t :heading "Notes")
+     ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\12 %i\12 %a" :prepend t :heading "Changelog")))
+ '(org-default-notes-file "/home/yoshiki01/.org/notes.org")
+ '(org-download-abbreviate-filename-function
+   '(closure
+        (t)
+        (path)
+      (if
+          (file-in-directory-p path org-download-image-dir)
+          (file-relative-name path org-download-image-dir)
+        path)))
+ '(org-download-heading-lvl nil)
+ '(org-download-method 'attach)
+ '(org-download-screenshot-method nil)
+ '(org-download-timestamp "_%Y%m%d_%H%M%S")
+ '(org-export-headline-levels 16)
+ '(org-freemind-section-format 'note)
+ '(org-goto-auto-isearch nil)
+ '(org-hide-emphasis-markers t)
+ '(org-hide-leading-stars t)
+ '(org-log-done nil)
+ '(org-num-face '(:inherit org-special-keyword :underline nil :weight bold))
+ '(org-pomodoro-audio-player "/usr/bin/paplay")
+ '(org-pomodoro-finished-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/startPomo.wav")
+ '(org-pomodoro-keep-killed-pomodoro-time t)
+ '(org-pomodoro-killed-sound nil)
+ '(org-pomodoro-killed-sound-p nil)
+ '(org-pomodoro-length 45)
+ '(org-pomodoro-long-break-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/startPomo.wav")
+ '(org-pomodoro-overtime-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/startPomo.wav")
+ '(org-pomodoro-short-break-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/startPomo.wav")
+ '(org-pomodoro-start-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/startPomo.wav")
+ '(org-pomodoro-start-sound-p t)
+ '(org-pomodoro-ticking-sound
+   "/home/yoshiki01/.emacs.d/.local/straight/build-29.4/org-pomodoro/resources/tick.wav")
+ '(org-pomodoro-ticking-sound-p nil)
+ '(org-refile-targets
+   '(("~/.org/done.org" :level . 3)
+     (nil :maxlevel . 3)
+     (org-agenda-files :level . 1)))
+ '(org-refile-use-outline-path 'file)
+ '(org-todo-keywords
+   '((sequence "TODO(t)" "PROJ(p)" "LOOP(r)" "STRT(s)" "WAIT(w)" "HOLD(h)" "IDEA(i)" "|" "DONE(d)" "KILL(k)")
+     (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
+     (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
+ '(org-use-tag-inheritance nil)
+ '(package-selected-packages '(winum))
+ '(tab-width 4)
+ '(which-key-use-C-h-commands t))
+;; NOTE myTheme的自定义face
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :extend nil :stipple nil :background "#EDEDED" :foreground "#2E3436" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 126 :width normal :foundry "ADBO" :family "Source Code Pro"))))
+;;  '(+org-todo-active ((t (:foreground "lime green"))))
+;;  '(+org-todo-cancel ((t (:foreground "black"))))
+;;  '(+org-todo-onhold ((t (:foreground "dark orange"))))
+;;  '(+org-todo-project ((t (:foreground "deep sky blue"))))
+;;  '(bold ((t (:foreground "black" :weight bold))))
+;;  '(cursor ((t (:background "black"))))
+;;  '(doom-modeline-buffer-file ((t (:foreground "gold" :weight bold))))
+;;  '(doom-modeline-buffer-path ((t (:foreground "gold"))))
+;;  '(error ((t (:foreground "red" :weight bold))))
+;;  '(highlight ((t (:background "gold" :foreground "black"))))
+;;  '(italic ((t (:slant italic))))
+;;  '(line-number ((t (:foreground "black" :weight bold))))
+;;  '(lsp-ui-doc ((t (:height 1.0 :family "Courier New"))))
+;;  '(lsp-ui-doc-background ((t (:background "white"))))
+;;  '(magit-section-highlight ((t (:extend t :background "dark orange"))))
+;;  '(mode-line ((t (:background "black" :foreground "white" :box (:line-width (1 . -1) :style released-button)))))
+;;  '(org-agenda-dimmed-todo-face ((t nil)))
+;;  '(org-agenda-done ((t (:foreground "RosyBrown" :weight bold))))
+;;  '(org-done ((t (:foreground "RosyBrown" :weight bold))))
+;;  '(org-inline-src-block ((t (:inherit org-block))))
+;;  '(org-list-dt ((t (:foreground "orange red" :weight bold))))
+;;  '(org-table ((t (:foreground "Blue1"))))
+;;  '(org-todo ((t (:foreground "deep pink" :weight bold))))
+;;  '(org-upcoming-deadline ((t (:foreground "red" :weight bold))))
+;;  '(org-upcoming-distant-deadline ((t (:foreground "red" :weight bold))))
+;;  '(org-warning ((t (:foreground "red" :weight bold))))
+;;  '(shadow ((t (:foreground "gray30")))))
+;; NOTE yoshikiDark的自定义face
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(+org-todo-project ((t (:foreground "deep pink" :weight bold))))
+ '(lsp-ui-doc ((t (:height 1.0 :family "Courier New"))))
+ '(org-agenda-dimmed-todo-face ((t nil)))
+ '(org-hide ((t nil)))
+ '(org-scheduled ((t (:foreground "lawn green"))))
+ '(org-scheduled-previously ((t (:foreground "sea green"))))
+ '(org-scheduled-today ((t (:foreground "lawn green"))))
+ '(org-upcoming-deadline ((t (:foreground "orange"))))
+ '(org-warning ((t (:foreground "orange red")))))
